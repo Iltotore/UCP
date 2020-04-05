3b7cb8faba8c7c726831e6b35f4134473759bfa2
Iltotore <rafbodaha@gmail.com>
2020-04-05 11:48:46 +0200

    Added a test (just for testing ScalaTest. Will be upgraded and cleaned later)
    Signed-off-by: Iltotore <rafbodaha@gmail.com>

---
+++ src/test/scala/fr/il_totore/ucp/CommandTest.scala
@@ -0,0 +1,20 @@
+package fr.il_totore.ucp
+
+import fr.il_totore.ucp.CommandResult._
+import fr.il_totore.ucp.CommandSpec.ImplicitSpec
+import org.scalatest.flatspec.AnyFlatSpec
+
+class CommandTest extends AnyFlatSpec {
+
+
+  "A CommandSpec" should "be consistent" in {
+    val aPermission: String => Boolean = name => name.equals("Il_totore")
+    val aCommandExecutor: (String, CommandContext) => CommandResult = (_: String, _: CommandContext) => null
+
+    val myCommand = "myCommand" describedAs "A test command" withPermission aPermission executing aCommandExecutor
+  }
+
+  "A CommandResult" should "be consistent" in {
+    val result = SUCCESS in "creating a result"
+  }
+}
