c31d1c83f8ae0a9b9db90c439b987d73b1b3a9ff
Iltotore <rafbodaha@gmail.com>
2020-04-19 14:30:59 +0200

    Updated tests.
    Signed-off-by: Iltotore <rafbodaha@gmail.com>

--- src/test/scala/fr/il_totore/ucp/CommandTest.scala
+++ src/test/scala/fr/il_totore/ucp/CommandTest.scala
@@ -2,7 +2,8 @@ package fr.il_totore.ucp
 
 import fr.il_totore.ucp.CommandResult._
 import fr.il_totore.ucp.CommandSpec.ImplicitSpec
-import fr.il_totore.ucp.parsing.CommandElement._
+import fr.il_totore.ucp.parsing.CommandElement.SequenceElement
+import fr.il_totore.ucp.parsing.EndElement._
 import fr.il_totore.ucp.parsing.{CommandArguments, CommandElement, ParsingResult}
 import fr.il_totore.ucp.registration.{CommandRegistry, PrefixedCommandRegistry}
 import org.scalatest.flatspec.AnyFlatSpec
@@ -12,14 +13,22 @@ import scala.collection.mutable.ListBuffer
 class CommandTest extends AnyFlatSpec {
 
 
+  def dummyExecutor(sender: String, context: CommandContext[String]): Unit = {
+
+  }
+
   def commandTest(): CommandSpec[String] = {
-    val aPermission: String => Boolean = name => name.equals("Il_totore")
-    val aCommandExecutor: (String, CommandContext[String]) => CommandResult = (sender: String, context: CommandContext[String]) => {
-      assert(context.getFirst[Boolean]("boolArg").getOrElse(false))
-      SUCCESS whilst "executing test command"
+    def dummyExecutor(sender: String, context: CommandContext[String]): CommandResult = {
+      if (!context.getFirst[Boolean]("boolArg").getOrElse(false)) return FAILURE whilst "asserting true"
+      SUCCESS whilst Math.pow(context.getFirst[Int]("intArg").getOrElse(0).toDouble, 2).toString
     }
-    val element: CommandElement[String] = "boolArg" lambda((sender: String, args: CommandArguments, context: CommandContext[String]) => true)
-    "myCommand" describedAs "A test command" withPermission aPermission executing aCommandExecutor withElement element
+
+    val aPermission: String => Boolean = name => name.equals("Il_totore")
+    val firstArg: CommandElement[String] = "boolArg" lambda((sender: String, args: CommandArguments, context: CommandContext[String]) =>
+      args.next.getOrElse("false").toBoolean)
+    val secondArg: CommandElement[String] = "intArg" lambda((sender: String, args: CommandArguments, context: CommandContext[String]) =>
+      args.next.getOrElse("-1").toInt)
+    "myCommand" describedAs "A test command" withPermission aPermission executing dummyExecutor requiring(firstArg and secondArg)
   }
 
   def createRegister(spec: CommandSpec[String]): CommandRegistry[String] = {
@@ -28,13 +37,18 @@ class CommandTest extends AnyFlatSpec {
     registry
   }
 
-  def executingTest(registry: CommandRegistry[String], sender: String, cmd: String): Unit = {
+  def executeTest(registry: CommandRegistry[String], sender: String, cmd: String): CommandResult = {
     val result: ParsingResult[String] = registry.parse(sender, cmd)
     assert(result.getContext.isDefined)
     result.getContext.get.execute(sender)
   }
 
 
-  "A Command" should "be consistent" in commandTest()
-  "A CommandRegistry" should "register and parse commands consistently" in executingTest(createRegister(commandTest()), "Il_totore", "/myCommand true")
+  //"A Command" should "be consistent" in commandTest()
+  "A CommandRegistry" should "register and parse commands consistently" in {
+    var result = executeTest(createRegister(commandTest()), "Il_totore", "/myCommand false")
+    assert(result.equals(FAILURE whilst "asserting true"))
+    result = executeTest(createRegister(commandTest()), "Il_totore", "/myCommand true 5")
+    assert(result.equals(SUCCESS whilst "25.0"))
+  }
 }
