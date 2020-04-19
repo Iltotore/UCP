8045e29260c89bc4e968f4ad7d6e938ce692e0c5
Iltotore <rafbodaha@gmail.com>
2020-04-19 15:32:48 +0200

    LambdaElement#lambda now returns a ParsingResult. To put arguments use CommandContext#put
    Signed-off-by: Iltotore <rafbodaha@gmail.com>

--- src/main/scala/fr/il_totore/ucp/parsing/EndElement.scala
+++ src/main/scala/fr/il_totore/ucp/parsing/EndElement.scala
@@ -2,7 +2,6 @@ package fr.il_totore.ucp.parsing
 
 import fr.il_totore.ucp.CommandContext
 import fr.il_totore.ucp.parsing.CommandElement.NamedElement
-import fr.il_totore.ucp.parsing.ParsingResult._
 
 abstract class EndElement[S](key: String) extends NamedElement[S](key: String) {
 
@@ -13,14 +12,11 @@ object EndElement {
 
   implicit class LambdaElement[S](key: String) extends EndElement[S](key: String) {
 
-    private var parseFunction: (S, CommandArguments, CommandContext[S]) => Any = _
+    private var parseFunction: (S, CommandArguments, CommandContext[S]) => ParsingResult[S] = _
 
-    override def parse(sender: S, arguments: CommandArguments, context: CommandContext[S]): ParsingResult[S] = {
-      context.putArgument(key, parseFunction.apply(sender, arguments, context))
-      SUCCESS parsing arguments in context
-    }
+    override def parse(sender: S, arguments: CommandArguments, context: CommandContext[S]): ParsingResult[S] = parseFunction.apply(sender, arguments, context)
 
-    def lambda(func: (S, CommandArguments, CommandContext[S]) => Any): LambdaElement[S] = {
+    def lambda(func: (S, CommandArguments, CommandContext[S]) => ParsingResult[S]): LambdaElement[S] = {
       this.parseFunction = func
       this
     }
