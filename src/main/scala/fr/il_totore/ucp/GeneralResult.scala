648cf738b542718ad7d341a8be16b3c12f44b1bc
Iltotore <rafbodaha@gmail.com>
2020-04-19 15:33:36 +0200

    Merged ParsingResult and CommandResult constants into GeneralResult
    Signed-off-by: Iltotore <rafbodaha@gmail.com>

---
+++ src/main/scala/fr/il_totore/ucp/GeneralResult.scala
@@ -0,0 +1,14 @@
+package fr.il_totore.ucp
+
+object GeneralResult {
+
+  /**
+   * Generic success code
+   */
+  val SUCCESS: Int = 0
+
+  /**
+   * Generic failure code
+   */
+  val FAILURE: Int = -1
+}
